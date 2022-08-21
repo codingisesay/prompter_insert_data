675fb8ca04f23b066d05db889589b5b84f339805
Akash Singh <akashsngh681681@gmail.com>
2022-08-21 16:52:52 +0530

    modifiy db_connect

--- db_connect.php
+++ db_connect.php
@@ -4,6 +4,6 @@ $user_name="root";
 $password="akash@1234";
 $database_name="vision_prompter";
 
-$connect = mysqli_connect($hostname, $user_name,$password,$database_name) or die("Connection Fail");
+$connect = mysqli_connect($hostname, $user_name,$password,$database_name) or die("Conection Fail");
 
 ?>
