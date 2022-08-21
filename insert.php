d73bf588b0112e5b1e8d8ec09d042093f9eabf73
Akash Singh <akashsngh681681@gmail.com>
2022-08-21 15:41:31 +0530

    Insert comment

--- insert.php
+++ insert.php
@@ -9,7 +9,7 @@ if(isset($_POST['submit']) & !empty($_POST['usertype']) & !empty($_POST['user_na
 	$status = 1;
 	$authorizedapp_list = 'qnNpte';
 	$user_type = $_POST['usertype'];
-	
+	//query for insert
 	$query = "INSERT INTO system_users (Name,username,password,status,authorizedapp_list,usertype)
 	VALUES ('$user_name','$user_mail_id','$user_password_hash',$status,'$authorizedapp_list','$user_type')";
 	$result = mysqli_query($connect,$query) or die('Query Falied');
